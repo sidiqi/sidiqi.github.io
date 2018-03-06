7a791aa7cb35fa78fc41e7c379ead2c4a2d8c23d
sidiqi <cssitemail@gmail.com>
2018-03-06 13:48:46 +0300

    upd

--- lessons/javascript/main.js
+++ lessons/javascript/main.js
@@ -4,19 +4,16 @@ var position = 0;
 var id;
 
 getCar.onmouseover = function (){
-	//console.log('over');
 	if(id) clearInterval(id);
 	id = setInterval(toDriveRight, 2)
 	}
 //снять наведения
 getCar.onmouseout = function (){
-		//console.log('out');
 	if(id) clearInterval(id);
 	id = setInterval(toDriveLeft, 2)
 }
 
 function toDriveLeft(){
-	//console.log('out');
 	if(position <= 0){
 		clearInterval(id);
 	}else{
@@ -27,11 +24,11 @@ function toDriveLeft(){
 }
 
 function toDriveRight(){
-		if(position >= 450){
+	if(position >= 450){
 			clearInterval(id);
-		}else{
-			position ++;
-			sedan.style.left = position + 'px';
-		}
+	}else{
+		position ++;
+		sedan.style.left = position + 'px';
+	}
 
 }
