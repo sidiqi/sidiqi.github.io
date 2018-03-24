e5db31c2dd77247342d2c8854562c4ad70280b6a
sidiqi <cssitemail@gmail.com>
2018-03-24 13:31:37 +0300

    ipd

---
+++ php.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+
+
+	<script type="text/javascript">
+		
+/*
+var x = prompt('Input x value' , 0); 
+var y = prompt("Input y value ", "0.0");
+var integerX = parseInt(x);
+var floatY = parseFloat(y);
+var result = integerX + floatY;
+document.write(result);
+*/
+
+	</script>
+</body>
+</html>
